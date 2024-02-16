e8f6a199a538bed502eb90993cd706b1d7636c3f
Max Brenner <mbrenner91@pm.me>
2024-02-17 00:13:56 +0100

    WIP: lexical editor

--- src/MessageEditor/MessageEditor.tsx
+++ src/MessageEditor/MessageEditor.tsx
@@ -1,11 +1,24 @@
 import {InitialConfigType, LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import {LexicalEditor} from "lexical";
 import EmojiPickerPlugin from './plugins/EmojiPickerPlugin';
 import EmojisPlugin from './plugins/EmojisPlugin';
 import {EmojiNode} from "./nodes/EmojiNode.tsx";
+import {
+  TRANSFORMERS,
+} from '@lexical/markdown';
+import { LinkNode } from '@lexical/link';
+import { ListNode, ListItemNode } from '@lexical/list';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
+import PlaygroundEditorTheme from '../themes/PlaygroundEditorTheme';
+
+
+import {MarkdownShortcutPlugin} from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import {RichTextPlugin} from "@lexical/react/LexicalRichTextPlugin";
+import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
+import {CodeHighlightNode, CodeNode} from '@lexical/code';
 
 export function MessageEditor() {
   const initialConfig: InitialConfigType = {
@@ -13,18 +26,39 @@ export function MessageEditor() {
       console.error('Error in editor', error, editor);
     },
     namespace: 'MyEditor',
-    nodes: [EmojiNode]
+    nodes: [
+      HorizontalRuleNode,
+      LinkNode,
+      ListNode,
+      ListItemNode,
+      HeadingNode,
+      QuoteNode,
+      EmojiNode,
+      CodeHighlightNode,
+      CodeNode,
+    ],
+    theme: PlaygroundEditorTheme,
   };
 
   return (
+    <div className="editor-container">
     <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable/>}
-        placeholder={<div>Enter some text...</div>}
+      <RichTextPlugin
+        contentEditable={
+          <div className="editor-scroller">
+            <div className="editor" >
+              <ContentEditable />
+            </div>
+          </div>
+        }
+        placeholder={<></>}
         ErrorBoundary={LexicalErrorBoundary}
       />
+      <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+      <CodeHighlightPlugin />
       <EmojiPickerPlugin />
       <EmojisPlugin />
     </LexicalComposer>
+    </div>
   );
 }

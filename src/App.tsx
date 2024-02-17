d188bd58d627c135ab54065b413823c813733098
Max Brenner <mbrenner91@pm.me>
2024-02-17 10:24:33 +0100

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import {MessageEditor} from "./MessageEditor/MessageEditor.tsx";
+import {MessageEditor} from "./MessageEditor2/MessageEditor.tsx";
 
 function App() {
   return (
-    <>
+    <div style={{
+      backgroundColor: 'red',
+      marginTop: 'calc(100vh - 100px)',
+      overflow: 'hidden',
+    }}>
       <MessageEditor />
-    </>
+    </div>
   )
 }
 
